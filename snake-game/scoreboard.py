6da76bdd166b1a04040cc33118db46eb7cf94663
lshihan <86920917+lshihan@users.noreply.github.com>
2021-09-14 19:25:51 +0800

    Add files via upload

--- snake-game/scoreboard.py
+++ snake-game/scoreboard.py
@@ -8,6 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("snake_score.txt") as file:
+            self.high_score = int(file.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -15,11 +17,20 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGN, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGN, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("Game Over!", align=ALIGN, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("snake_score.txt",mode="w") as file:
+                file.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("Game Over!", align=ALIGN, font=FONT)
 
     def increase_score(self):
         self.score += 1
--- snake-game/snake.py
+++ snake-game/snake.py
@@ -25,6 +25,13 @@ class Snake:
         t.goto(position)
         self.segment.append(t)
 
+    def reset(self):
+        for seg in self.segment:
+            seg.goto(1000,1000)
+        self.segment.clear()
+        self.create_snake()
+        self.head = self.segment[0]
+
     def extend(self):
         self.add_segment(self.segment[-1].position())
 
